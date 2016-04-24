a5c185485b6285a9eb00d8991b1eb6205e9ce166
M.D.Ward <dev@mattdw.co.uk>
2016-04-24 14:26:05 +0100

    Added DefaultParameterValueStrategy.

---
+++ src/Mdjward/LaravelContainerExtensions/Container/DependencyResolutionStrategy/DefaultParameterValueStrategy.php
@@ -0,0 +1,48 @@
+<?php
+
+/**
+ * DefaultParameterValueStrategy.php
+ * Definition of class DefaultParameterValueStrategy
+ * 
+ * Created 21-Apr-2016 21:48:42
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy;
+
+use Illuminate\Contracts\Container\Container;
+use ReflectionParameter;
+
+/**
+ * DefaultParameterValueStrategy
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+class DefaultParameterValueStrategy implements ResolutionStrategyInterface
+{
+
+    /**
+     * 
+     * @param Container $container
+     * @param ReflectionParameter $parameterToMatch
+     * @param array $givenParameters
+     * @param array $givenPrimitives
+     * @return mixed
+     * @throws ResolutionFailedException
+     */
+    public function resolveParameter(
+        Container $container,
+        ReflectionParameter $parameterToMatch,
+        array $givenParameters = [],
+        array $givenPrimitives = [])
+    {
+        if ($parameterToMatch->isDefaultValueAvailable()) {
+            return $parameterToMatch->getDefaultValue();
+        }
+
+        throw new ResolutionFailedException($parameterToMatch);
+    }
+    
+}
---
+++ tests/Mdjward/LaravelContainerExtensions/Container/DependencyResolutionStrategy/DefaultParameterValueStrategyTest.php
@@ -0,0 +1,100 @@
+<?php
+
+/**
+ * DefaultParameterValueStrategyTest.php
+ * Definition of class DefaultParameterValueStrategyTest
+ * 
+ * Created 22-Apr-2016 09:00:58
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy;
+
+
+
+/**
+ * DefaultParameterValueStrategyTest
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * @coversDefaultClass \Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy\DefaultParameterValueStrategy
+ */
+class DefaultParameterValueStrategyTest extends AbstractStrategyTestCase
+{
+    
+    /**
+     *
+     * @var DefaultParameterValueStrategy
+     */
+    private $strategy;
+    
+    
+    
+    /**
+     * 
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->strategy = new DefaultParameterValueStrategy();
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     */
+    public function testResolveParameterReturnsDefaultValueIfAvailable()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isDefaultValueAvailable')
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getDefaultValue')
+            ->will($this->returnValue(($returnValue = "RETURN VALUE")))
+        ;
+        
+        $this->assertSame(
+            $returnValue,
+            $this->strategy->resolveParameter(
+                $this->container,
+                $this->parameterToMatch
+            )
+        );
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     */
+    public function testResolveParameterThrowsExceptionIfUnableToResolve()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isDefaultValueAvailable')
+            ->will($this->returnValue(false))
+        ;
+
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue(($parameterName = 'PARAMETER NAME')))
+        ;
+
+        $this->setExpectedException(
+            ResolutionFailedException::class,
+            "Failed to resolve parameter " . $parameterName
+        );
+
+        $this->strategy->resolveParameter(
+            $this->container,
+            $this->parameterToMatch
+        );
+    }
+    
+}
