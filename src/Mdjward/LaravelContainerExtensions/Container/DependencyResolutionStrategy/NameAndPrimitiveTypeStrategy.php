ab23c0888fec5d9e8c43999efdb01ccc6fb39a20
M.D.Ward <dev@mattdw.co.uk>
2016-04-24 15:20:23 +0100

    Added NameAndPrimitiveTypeStrategy.

---
+++ src/Mdjward/LaravelContainerExtensions/Container/DependencyResolutionStrategy/NameAndPrimitiveTypeStrategy.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * NameAndPrimitiveTypeStrategy.php
+ * Definition of class NameAndPrimitiveTypeStrategy
+ * 
+ * Created 21-Apr-2016 21:50:33
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy;
+
+use Illuminate\Contracts\Container\Container;
+use ReflectionParameter;
+
+/**
+ * NameAndPrimitiveTypeStrategy
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+class NameAndPrimitiveTypeStrategy
+{
+
+    /**
+     * 
+     * @param Container $container
+     * @param ReflectionParameter $parameterToMatch
+     * @param array $givenParameters
+     * @param array $givenPrimitives
+     * @return mixed
+     * @throws ResolutionFailedException
+     */
+    public function resolveParameter(
+        Container $container,
+        ReflectionParameter $parameterToMatch,
+        array $givenParameters = array(),
+        array $givenPrimitives = array()
+    ) {
+        if ($parameterToMatch->isArray()) {
+            return $this->validateAndResolve(
+                $container,
+                $parameterToMatch,
+                'is_array'
+            );
+        }
+
+        if ($parameterToMatch->isCallable()) {
+            return $this->validateAndResolve(
+                $container,
+                $parameterToMatch,
+                'is_callable'
+            );
+        }
+        
+        if ($parameterToMatch->getClass() === null) {
+            return $this->validateAndResolve(
+                $container,
+                $parameterToMatch,
+                'is_scalar'
+            );
+        }
+
+        throw new ResolutionFailedException($parameterToMatch);
+    }
+
+    /**
+     * 
+     * @param Container $container
+     * @param ReflectionParameter $parameter
+     * @param callable $validationFunction
+     * @param string $retrievalMethod
+     * @return mixed
+     * @throws ResolutionFailedException
+     */
+    protected function validateAndResolve(
+        Container $container,
+        ReflectionParameter $parameter,
+        callable $validationFunction = null
+    ) {
+        $parameterName = $parameter->getName();
+        
+        if (
+            $container->bound($parameterName)
+            && ($containedValue = $container->make($parameterName)) !== null
+            && (
+                $validationFunction === null
+                || call_user_func($validationFunction, $containedValue) === true
+            )
+        ) {
+            return $containedValue;
+        }
+
+        throw new ResolutionFailedException($parameter);
+    }
+
+}
---
+++ tests/Mdjward/LaravelContainerExtensions/Container/DependencyResolutionStrategy/NameAndPrimitiveTypeStrategyTest.php
@@ -0,0 +1,241 @@
+<?php
+
+/**
+ * NameAndPrimitiveTypeStrategyTest.php
+ * Definition of class NameAndPrimitiveTypeStrategyTest
+ * 
+ * Created 24-Apr-2016 14:26:56
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy;
+
+
+
+/**
+ * NameAndPrimitiveTypeStrategyTest
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * @coversDefaultClass \Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy\NameAndPrimitiveTypeStrategy
+ */
+class NameAndPrimitiveTypeStrategyTest extends AbstractStrategyTestCase
+{
+    
+    /**
+     *
+     * @var NameAndPrimitiveTypeStrategy
+     */
+    private $strategy;
+    
+    
+    
+    protected function setUp()
+    {
+        parent::setUp();
+        
+        $this->strategy = new NameAndPrimitiveTypeStrategy();
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     * @covers ::validateAndResolve
+     */
+    public function testResolveParameterResolvesArrayHintedValue()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isArray')
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue(($parameterName = 'VALID ARRAY PARAMETER')))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('bound')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('make')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(($containedValue = [1, 2, 3, 4, 5])))
+        ;
+        
+        $this->assertSame(
+            $containedValue,
+            $this->strategy->resolveParameter(
+                $this->container,
+                $this->parameterToMatch
+            )
+        );
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     * @covers ::validateAndResolve
+     */
+    public function testResolveParameterFailsIfUnableToValidateCallableValue()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isArray')
+            ->will($this->returnValue(false))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isCallable')
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->exactly(2))
+            ->method('getName')
+            ->will($this->returnValue(($parameterName = 'INVALID callable PARAMETER')))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('bound')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('make')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(($containedValue = 12345)))
+        ;
+        
+        $this->setExpectedException(
+            ResolutionFailedException::class,
+            "Failed to resolve parameter " . $parameterName
+        );
+        
+        $this->strategy->resolveParameter(
+            $this->container,
+            $this->parameterToMatch
+        );
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     * @covers ::validateAndResolve
+     */
+    public function testResolveParameterResolvesPresumablyScalarValue()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isArray')
+            ->will($this->returnValue(false))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isCallable')
+            ->will($this->returnValue(false))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getClass')
+            ->will($this->returnValue(null))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue(($parameterName = 'VALID MISC PARAMETER')))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('bound')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(true))
+        ;
+        
+        $this->container
+            ->expects($this->once())
+            ->method('make')
+            ->with(
+                $this->identicalTo($parameterName)
+            )
+            ->will($this->returnValue(($containedValue = 12345)))
+        ;
+        
+        $this->assertSame(
+            $containedValue,
+            $this->strategy->resolveParameter(
+                $this->container,
+                $this->parameterToMatch
+            )
+        );
+    }
+    
+    /**
+     * @test
+     * @covers ::resolveParameter
+     * @covers ::validateAndResolve
+     */
+    public function testResolveParameterFailsIfNotArrayCallableOrScalar()
+    {
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isArray')
+            ->will($this->returnValue(false))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('isCallable')
+            ->will($this->returnValue(false))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getClass')
+            ->will($this->returnValue('STRING'))
+        ;
+        
+        $this->parameterToMatch
+            ->expects($this->once())
+            ->method('getName')
+            ->will($this->returnValue(($parameterName = 'INVALID UNKNOWN PARAMETER')))
+        ;
+        
+        $this->setExpectedException(
+            ResolutionFailedException::class,
+            "Failed to resolve parameter " . $parameterName
+        );
+        
+        $this->strategy->resolveParameter(
+            $this->container,
+            $this->parameterToMatch
+        );
+    }
+    
+}
