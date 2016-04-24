7a6c75c929056d14300c5e281f19e6de9f960dcf
M.D.Ward <dev@mattdw.co.uk>
2016-04-24 16:22:54 +0100

    Added ResolutionStrategyInterface contract.

---
+++ src/Mdjward/LaravelContainerExtensions/Container/DependencyResolutionStrategy/ResolutionStrategyInterface.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * ResolutionStrategyInterface.php
+ * Definition of interface ResolutionStrategyInterface
+ * 
+ * Created 21-Apr-2016 21:32:29
+ *
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ * Copyright (c) 2016, 
+ */
+
+namespace Mdjward\LaravelContainerExtensions\Container\DependencyResolutionStrategy;
+
+use Illuminate\Contracts\Container\Container;
+use ReflectionParameter;
+
+
+
+/**
+ * ResolutionStrategyInterface
+ * 
+ * @author M.D.Ward <dev@mattdw.co.uk>
+ */
+interface ResolutionStrategyInterface
+{
+    
+    /**
+     * 
+     * @param Container $container
+     * @param ReflectionParameter $parameterToMatch
+     * @param array $givenParameters
+     * @param array $givenPrimitives
+     */
+    public function resolveParameter(
+        Container $container,
+        ReflectionParameter $parameterToMatch,
+        array $givenParameters = [],
+        array $givenPrimitives = []
+    );
+    
+}
